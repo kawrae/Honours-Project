c1cf3bb83393c282cd23e12c92da61a647f261c2
Corey Black <coreyblack@hotmail.co.uk>
2026-03-08 18:32:29 +0000

    laravel tweak
    -kernel
    -db
    -api

--- apps/laravel/routes/api.php
+++ apps/laravel/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ItemController;
 
-Route::get('/items', [ItemController::class, 'index']);
-Route::get('/items/{id}', [ItemController::class, 'show']);
-Route::post('/items', [ItemController::class, 'store']);
-Route::put('/items/{id}', [ItemController::class, 'update']);
-Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+Route::withoutMiddleware(['throttle:api'])->group(function () {
+    Route::get('/items', [ItemController::class, 'index']);
+    Route::get('/items/{id}', [ItemController::class, 'show']);
+    Route::post('/items', [ItemController::class, 'store']);
+    Route::put('/items/{id}', [ItemController::class, 'update']);
+    Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+});
